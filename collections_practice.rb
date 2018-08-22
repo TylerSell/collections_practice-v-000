f041caad7e7bf53ccca92504ebb5353625b4a17c
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-22 03:05:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+def sort_array_asc(ascending)
+  ascending.sort do |a, b|
+    a <=> b 
+  end
+end
 
+def sort_array_desc(decending)
+  decending.sort do |a, b|
+    if a = b 
+      0 
+    elsif a > b 
+      1 
+    elsif a < b 
+      -1 
+    end
+  end
+end
+    
